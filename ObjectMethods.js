a3a363a91bbf45fd8d118ef5144b18b623bf51cb
Rishabh <rishabhgour157@gmail.com>
2022-08-13 01:49:01 +0530

    Name and comments deleted

--- ObjectMethods.js
+++ ObjectMethods.js
@@ -11,25 +11,25 @@ const obj = {
 }
 
 Object.freeze(obj)
-obj.actor = "RISHABH"
-// console.log(obj)
+obj.actor = "Christine bell"
+console.log(obj)
 
 
 /*** Object.keys ***/
 // Object.keys(obj) returns an array containing all the keys of the object
 
-// console.log(Object.keys(obj))
+console.log(Object.keys(obj))
 
 
 
 /*** Object.values ***/
 // Object.values(obj) returns an array containing all the values of the object
 
-// console.log(Object.values(obj))
+console.log(Object.values(obj))
 
 
 
 /*** Object.entries ***/
 // Object.entries(obj) returns both the key and its values in the form of nested array
 
-// console.log(Object.entries(obj))
+console.log(Object.entries(obj))
