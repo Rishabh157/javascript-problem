c7f67ecd57c854db0c530a442a146dc9d5e21389
Rishabh <rishabhgour157@gmail.com>
2022-08-13 01:47:08 +0530

    ObjectMethod addedd

---
+++ ObjectMethods.js
@@ -0,0 +1,35 @@
+
+/*** Object.freeze ***/
+// Object.freeze(obj) it prevents any changes to the object passed
+
+const obj = {
+ id: 1111,
+ char: "Joker",
+ actor: "Heath Ledger",
+ movei: "Dark Night",
+ director: "Christhopher Nolan"
+}
+
+Object.freeze(obj)
+obj.actor = "RISHABH"
+// console.log(obj)
+
+
+/*** Object.keys ***/
+// Object.keys(obj) returns an array containing all the keys of the object
+
+// console.log(Object.keys(obj))
+
+
+
+/*** Object.values ***/
+// Object.values(obj) returns an array containing all the values of the object
+
+// console.log(Object.values(obj))
+
+
+
+/*** Object.entries ***/
+// Object.entries(obj) returns both the key and its values in the form of nested array
+
+// console.log(Object.entries(obj))
