0066714c51b8c7d77f2081f42b76519569705450
Rishabh157 <rishabhgour157@gmail.com>
2022-12-01 15:02:34 +0530

    includes file added

---
+++ Includes.js
@@ -0,0 +1,6 @@
+//  includes() =>  includes methods return true if presence of element are finded in the array. otherwise it return false 
+
+const colorsArr = ["red", "blue", "green", "yellow"]
+
+const isAvailableColor = colorsArr.includes("yellow")
+console.log(isAvailableColor)
